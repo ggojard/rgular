786467f66a458ca23d091ff832390fda9aa3e376
Guillaume Gojard <ggojard@gmail.com>
2016-04-20 14:58:43 +0200

    add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'home#index'
+  get 'comments/index'
+  get 'comments/' => 'comments#view'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
