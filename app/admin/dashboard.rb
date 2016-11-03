ad9f27b0c9a250cf1853d49fe204963d320a08be
Gojard Guillaume <g.gojard@lectra.com>
2016-11-03 13:10:50 +0100

    minor change in HomeController.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -12,16 +12,16 @@ ActiveAdmin.register_page "Dashboard" do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Contexts" do
+          ul do
+            Context.recent(5).map do |context|
+              li link_to(context.title, admin_context_path(context))
+            end
+          end
+        end
+      end
 
     #   column do
     #     panel "Info" do
