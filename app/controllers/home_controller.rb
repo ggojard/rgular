676af9e9e64b9a7d6e1ea4490deafbaf5c17b982
Gojard Guillaume <g.gojard@lectra.com>
2016-11-03 11:55:04 +0100

    minor change in HomeController.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
   inherit_resources
   respond_to :html, :json, :xml
 
-  def index
-  	render html: "HomeController html render !"
-  end
+  # def index
+  # 	render html: "HomeController html render !"
+  # end
 
 end
